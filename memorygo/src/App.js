6612c0f5de9097365b696a002291f334ba01d6fa
AimceptionGian <67605487+AimceptionGian@users.noreply.github.com>
2024-02-24 03:23:36 +0100

    added another function to buttonClick

--- memorygo/src/App.js
+++ memorygo/src/App.js
@@ -9,6 +9,15 @@ const [countT2, setCountT2] = useState(0);
 const [round, setRound] = useState(1);
 const [team2btn, setTeam2btn] = useState(true);
 
+function btn1click() {
+  incCountT1();
+  rounds();
+}
+
+function btn2click() {
+  incCountT2();
+  rounds();
+}
 
 function incCountT1() {
   if(countT1 < 12) {
@@ -28,7 +37,7 @@ function incCountT2() {
   }
 }
 
-const rounds = () => {
+function rounds() {
   if (round%2 > 0) {
 
   }
@@ -39,14 +48,14 @@ let progressWidth = 500;
     <div>
       <p>Guest</p>
       <input id='team1' type="text" placeholder='Team1'></input>
-      <input id='team1btn' type="button" onClick={ incCountT1 } placeholder='Points Team1'></input>
+      <input id='team1btn' type="button" onClick={btn1click} placeholder='Points Team1'></input>
       <p>Points {countT1}</p>
       <div id='progress1Bg' style={{width: progressWidth , height: '20px', backgroundColor: 'grey'}}>
         <div id='progress2' style={{width: progressWidth/12*countT1 , height: '20px', backgroundColor: 'lightblue'}}></div>
       </div>
       <p>Home</p>
       <input id='team2' type="text" placeholder='Team2'></input>
-      <input id='team2btn' type="button" onClick={incCountT2} placeholder='Points Team2'></input>
+      <input id='team2btn' type="button" onClick={btn2click} placeholder='Points Team2'></input>
       <p>Points {countT2}</p>
       <div id='progress2Bg' style={{width: progressWidth , height: '20px', backgroundColor: 'grey'}}>
         <div id='progress1' style={{width: progressWidth/12*countT2 , height: '20px', backgroundColor: 'lightblue'}}></div>

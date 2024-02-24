fb0a7ba5d3d4c1d27acb9da6c708143ddf0a83ad
AimceptionGian <67605487+AimceptionGian@users.noreply.github.com>
2024-02-24 03:16:38 +0100

    switched const to function
    because i dont fully understand const abc = () => yet

--- memorygo/src/App.js
+++ memorygo/src/App.js
@@ -6,16 +6,20 @@ function App() {
 const [countT1, setCountT1] = useState(0);
 const [countT2, setCountT2] = useState(0);
 
-const incCountT1 = () => {
+const [round, setRound] = useState(1);
+const [team2btn, setTeam2btn] = useState(true);
+
+
+function incCountT1() {
   if(countT1 < 12) {
     setCountT1(countT1 + 3);
     //console.log(countT2);
   } else {
     console.log('T1 hat gewonnen');
   }
-}
+} 
 
-const incCountT2 = () => {
+function incCountT2() {
   if(countT2 < 12) {
     setCountT2(countT2 + 3);
     //console.log(countT2);
@@ -24,26 +28,31 @@ const incCountT2 = () => {
   }
 }
 
-let progressWidth = 500;
+const rounds = () => {
+  if (round%2 > 0) {
 
+  }
+}
+
+let progressWidth = 500;
   return (
     <div>
       <p>Guest</p>
       <input id='team1' type="text" placeholder='Team1'></input>
-      <input type="button" onClick={ incCountT1 } placeholder='Points Team1'></input>
-      <p>{countT1}</p>
+      <input id='team1btn' type="button" onClick={ incCountT1 } placeholder='Points Team1'></input>
+      <p>Points {countT1}</p>
       <div id='progress1Bg' style={{width: progressWidth , height: '20px', backgroundColor: 'grey'}}>
         <div id='progress2' style={{width: progressWidth/12*countT1 , height: '20px', backgroundColor: 'lightblue'}}></div>
       </div>
       <p>Home</p>
       <input id='team2' type="text" placeholder='Team2'></input>
-      <input type="button" onClick={incCountT2} placeholder='Points Team2'></input>
-      <p>{countT2}</p>
+      <input id='team2btn' type="button" onClick={incCountT2} placeholder='Points Team2'></input>
+      <p>Points {countT2}</p>
       <div id='progress2Bg' style={{width: progressWidth , height: '20px', backgroundColor: 'grey'}}>
         <div id='progress1' style={{width: progressWidth/12*countT2 , height: '20px', backgroundColor: 'lightblue'}}></div>
       </div>
     </div>
-  );  
+  );
 }
 
 export default App;

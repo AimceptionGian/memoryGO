9222180c4be2da64907b0a86e4b4b312c9da680e
AimceptionGian <67605487+AimceptionGian@users.noreply.github.com>
2024-02-24 01:49:36 +0100

    display count in p

--- memorygo/src/App.js
+++ memorygo/src/App.js
@@ -3,18 +3,29 @@ import react, {useState} from 'react';
 
 function App() {
 
-const [count, setCount] = useState(0);
+const [countT1, setCountT1] = useState(0);
+const [countT2, setCountT2] = useState(0);
 
-const incCount = () => {
-  setCount(count + 1);
-  console.log(count);
+const incCountT1 = () => {
+  setCountT1(countT1 + 3);
+  //console.log(countT1);
+}
+
+const incCountT2 = () => {
+  setCountT2(countT2 + 3);
+  //console.log(countT2);
 }
 
   return (
     <div>
-      <input type="text" placeholder='Team1'></input>
-      <input type="text" placeholder='Team2'></input>
-      <input type="button" onClick={ incCount }></input>
+      <p>Guest</p>
+      <input id='team1' type="text" placeholder='Team1'></input>
+      <input type="button" onClick={ incCountT1 } placeholder='Points Team1'></input>
+      <p>{countT1}</p>
+      <p>Home</p>
+      <input id='team2' type="text" placeholder='Team2'></input>
+      <input type="button" onClick={ incCountT2 } placeholder='Points Team2'></input>
+      <p>{countT2}</p>
     </div>
   );  
 }

8570967a030e07c77c0dae21d75b598e956ec284
AimceptionGian <67605487+AimceptionGian@users.noreply.github.com>
2024-02-25 01:58:11 +0100

    refactored

--- memorygo/src/App.js
+++ memorygo/src/App.js
@@ -9,48 +9,38 @@ const [countT2, setCountT2] = useState(0);
 const [team1btnState, setTeam1btnState] = useState(false);
 const [team2btnState, setTeam2btnState] = useState(false);
 
-function btn1click() {
+function shoot(team) {
   if(Math.floor(Math.random() * 11) >= 5) {
-    incCountT1();
-    document.getElementById("status").value += "Basket made by the Guests \n";
-  } else {
-    document.getElementById("status").value += "Guets miss the basket \n";
-  }
-  setTeam1btnState(true);
-  setTeam2btnState(false);
-}
-
-function btn2click() {
-  if(Math.floor(Math.random() * 11) >= 5) {
-    incCountT2();
-    document.getElementById("status").value += "Basket made by the home team \n";
-  } else {
-    document.getElementById("status").value += "Home team miss the basket \n";
-  }
-  setTeam2btnState(true);
-  setTeam1btnState(false);
-}
-
-function incCountT1() {
-  setCountT1(countT1 + 3);
-  if(countT1 + 3 == 12) {
-    if(document.getElementById("team1").value.trim() === "") {
-      gameWon("Team 1");
-    } else {
-      gameWon(document.getElementById("team1").value);
-      console.log('T1 hat gewonnen');
+    if(team === "team1") {
+      setCountT1(countT1 + 3);
+      document.getElementById("status").value += "Basket made by the Guests \n";
+      setTeam1btnState(true);
+      setTeam2btnState(false);
+      if(countT1 + 3 == 12) {
+        if(document.getElementById("team1").value.trim() === "") {
+          gameWon("Team 1");
+        } else {
+          gameWon(document.getElementById("team1").value);
+        }
+      }
+    } else if(team === "team2") {
+      setCountT2(countT2 + 3);
+      document.getElementById("status").value += "Basket made by the home team \n";
+      setTeam2btnState(true);
+      setTeam1btnState(false);
+      if(countT2 + 3 == 12) {
+        if(document.getElementById("team2").value.trim() === "") {
+          gameWon("Team 2");
+        } else {
+          gameWon(document.getElementById("team2").value);
+        }
+      }
     }
-  }
-} 
-
-function incCountT2() {
-  setCountT2(countT2 + 3);
-  if(countT2 + 3 == 12) {
-    if(document.getElementById("team2").value.trim() === "") {
-      gameWon("Team 2");
-    } else {
-      gameWon(document.getElementById("team2").value);
-      console.log('T2 hat gewonnen');
+  } else {
+    if(team === "team1") {
+      document.getElementById("status").value += "Guets miss the basket \n";
+    } else if(team === "team2") {
+      document.getElementById("status").value += "Home team miss the basket \n";
     }
   }
 }
@@ -69,7 +59,7 @@ let size = 50;
         <div className='flex-item'>
           <p className='flex-item'>Guest</p>
           <input className='flex-item' id='team1' type="text" placeholder='Team1'></input>
-          <button className='flex-item' id='team1btn' type="button" style={{width: size, height: size}} onClick={btn1click} disabled={team1btnState || countT1 >= 12 || countT2 >= 12} placeholder='Points Team1'>Shoot</button>
+          <button className='flex-item' id='team1btn' type="button" style={{width: size, height: size}} onClick={() => shoot("team1")} disabled={team1btnState || countT1 >= 12 || countT2 >= 12} placeholder='Points Team1'>Shoot</button>
           <p className='flex-item'>Points {countT1}</p>
           <div className='flex-item' id='progress1Bg' style={{width: progressWidth , height: '20px', backgroundColor: 'grey'}}>
             <div id='progress2' style={{width: progressWidth/12*countT1 , height: '20px', backgroundColor: 'lightblue'}}></div>
@@ -83,7 +73,7 @@ let size = 50;
         <div className='flex-item'>
           <p className='flex-item'>Home</p>
           <input className='flex-item' id='team2' type="text" placeholder='Team2'></input>
-          <button className='flex-item' id='team2btn' type="button" style={{width: size, height: size}} onClick={btn2click} disabled={team2btnState || countT1 >= 12 || countT2 >= 12} placeholder='Points Team2'>Shoot</button>
+          <button className='flex-item' id='team2btn' type="button" style={{width: size, height: size}} onClick={() => shoot("team2")} disabled={team2btnState || countT1 >= 12 || countT2 >= 12} placeholder='Points Team2'>Shoot</button>
           <p className='flex-item'>Points {countT2}</p>
           <div className='flex-item' id='progress2Bg' style={{width: progressWidth , height: '20px', backgroundColor: 'grey'}}>
             <div id='progress1' style={{width: progressWidth/12*countT2 , height: '20px', backgroundColor: 'lightblue'}}></div>
